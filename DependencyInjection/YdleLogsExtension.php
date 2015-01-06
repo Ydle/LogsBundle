d714dbed82a8cec360422b2272df8ceb718e1510
Yaug <manuel.esteban@chroniques-lunaires.com>
2015-01-06 06:02:44 +0100

    Remove xml config form yaml configuration

--- DependencyInjection/YdleLogsExtension.php
+++ DependencyInjection/YdleLogsExtension.php
@@ -37,8 +37,8 @@ class YdleLogsExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-        $loader->load('forms.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+        $loader->load('forms.yml');
     }
 }
